d3a1e56505488f02b18f07fa5f7dd4fa218d9cb3
Kuzi2323 <kuzi2323@gmail.com>
2020-12-15 02:12:06 -0800

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,16 @@
+def speak_to_grandma(phrase)
+if phrase == "I LOVE YOU GRANDMA!"
+   "I LOVE YOU TOO PUMPKIN!"
+elsif phrase == "WHAT DID YOU EAT TODAY?"
+    "NO, NOT SINCE 1938!"
+elsif phrase == "WHAT?"
+    "NO, NOT SINCE 1938!"
+else 
+    "HUH?! SPEAK UP, SONNY!"
+end
+end 
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
